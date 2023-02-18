3760ce7cec379069bfe50bde1875e79c5e0aa329
Nedrag <92254461+Nedrag@users.noreply.github.com>
2023-02-18 16:22:39 +0100

    Added button functionaly for the calculator

--- calculator/index.js
+++ calculator/index.js
@@ -28,6 +28,7 @@ const btnPerecent = document.querySelector("#perecent");
 const btnDivide = document.querySelector("#divide");
 //Result
 const result = document.querySelector("#result");
+const currentSign = document.querySelector("#sign");
 
 //Event handles for numbers
 btnZero.addEventListener("click", () => handleButtonNumber(btnZero));
@@ -40,36 +41,125 @@ btnSix.addEventListener("click", () => handleButtonNumber(btnSix));
 btnSeven.addEventListener("click", () => handleButtonNumber(btnSeven));
 btnEight.addEventListener("click", () => handleButtonNumber(btnEight));
 btnNine.addEventListener("click", () => handleButtonNumber(btnNine));
+//Event for utility buttons
+btnAC.addEventListener("click", () => clearScreen());
+btnPlus.addEventListener("click", () => handleArithmetic(btnPlus));
+btnEquals.addEventListener("click", () => handleArithmetic(btnEquals));
+btnTimes.addEventListener("click", () => handleArithmetic(btnTimes));
+btnDivide.addEventListener("click", () => handleArithmetic(btnDivide));
+btnMinus.addEventListener("click", () => handleArithmetic(btnMinus));
 
 //Operation handle
-let OPERATION_MODE = false;
-let OPERATOR1 = 0;
-let OPERATOR2 = 0;
+let SECOND_OPERAND = false;
+let OPERATOR1 = ""; //Current string of numbers being displayed
+let OPERATOR2 = ""; // Only used when it needs to do arithmetic
+let PREVIOUS_BUTTON = "";
 
 function handleButtonNumber(btn) {
-	if (OPERATION_MODE) {
+	if (result.textContent.trim().length >= 8) {
 		return;
-	}
-	const num = btn.textContent;
-	if (result.textContent === "0") {
-		result.textContent = num;
+	} // Fit check
+	if (SECOND_OPERAND) {
+		//console.log("INSIDE");
+		OPERATOR2 = handleDisplay(btn, OPERATOR2);
+		result.textContent = OPERATOR2;
 		return;
+	} //Second operand
+
+	//console.log("OUTSIDE");
+	OPERATOR1 = handleDisplay(btn, OPERATOR1);
+	result.textContent = OPERATOR1;
+	return; //First operand
+}
+
+function handleDisplay(btn, operator) {
+	return operator + btn.textContent.trim();
+}
+
+function clearScreen() {
+	//console.log(OPERATOR1);
+	//console.log(OPERATOR2);
+	OPERATOR1 = "";
+	OPERATOR2 = "";
+	currentSign.textContent = "";
+	result.textContent = "";
+	SECOND_OPERAND = false;
+}
+
+function handleArithmetic(btn) {
+	if (btn.textContent.trim() !== "=") {
+		PREVIOUS_BUTTON = btn.textContent.trim(); //Keeping track of the previous
+		//button for equals operation
 	}
-	//console.log(num.length);
-	//console.log(result.textContent);
-	result.textContent = result.textContent + num;
-	let resultLength = result.textContent.replace(/^\s+|\s+$/gm, "");
-	console.log(resultLength.length);
-	//Length goes up by two for some reason
-	switch (resultLength.length) {
-		case 11:
-			result.style.fontSize = "60pt";
-			break;
-		case 15:
-			result.style.fontSize = "48pt";
-			break;
-		case 17:
-			result.style.fontSize = "36pt";
-			break;
+	let OPERATION_MODE = btn.textContent.trim(); //Removes whitespace
+	currentSign.textContent = OPERATION_MODE; //Changes the text so it displays the sign
+	SECOND_OPERAND = true;
+	console.log("right");
+	let x = parseFloat(OPERATOR1);
+	let y = parseFloat(OPERATOR2);
+	if (OPERATOR1 !== "" && OPERATOR2 !== "") {
+		switch (OPERATION_MODE) {
+			case "+":
+				OPERATOR1 = x + y; //Do the operation
+				OPERATOR2 = ""; //Empty out
+				result.textContent = OPERATOR1; //Display
+				currentSign.textContent = ""; //Changes the text so it displays the sign
+				break;
+			case "-":
+				OPERATOR1 = x - y; //Do the operation
+				OPERATOR2 = ""; //Empty out
+				result.textContent = OPERATOR1; //Display
+				currentSign.textContent = ""; //Changes the text so it displays the sign
+				break;
+			case "x":
+				OPERATOR1 = x * y; //Do the operation
+				OPERATOR2 = ""; //Empty out
+				result.textContent = OPERATOR1; //Display
+				currentSign.textContent = ""; //Changes the text so it displays the sign
+				break;
+			case "/":
+				if (y == 0) {
+					result.textContent = "ERROR";
+				}
+				OPERATOR1 = x / y; //Do the operation
+				OPERATOR2 = ""; //Empty out
+				result.textContent = OPERATOR1; //Display
+				currentSign.textContent = ""; //Changes the text so it displays the sign
+				break;
+			case "=":
+				switch (PREVIOUS_BUTTON) {
+					case "+":
+						OPERATOR1 = x + y; //Do the operation
+						OPERATOR2 = ""; //Empty out
+						result.textContent = OPERATOR1; //Display
+						currentSign.textContent = ""; //Changes the text so it displays the sign
+						PREVIOUS_BUTTON = "";
+						break;
+					case "-":
+						OPERATOR1 = x - y; //Do the operation
+						OPERATOR2 = ""; //Empty out
+						result.textContent = OPERATOR1; //Display
+						currentSign.textContent = ""; //Changes the text so it displays the sign
+						PREVIOUS_BUTTON = "";
+						break;
+					case "x":
+						OPERATOR1 = x * y; //Do the operation
+						OPERATOR2 = ""; //Empty out
+						result.textContent = OPERATOR1; //Display
+						currentSign.textContent = ""; //Changes the text so it displays the sign
+						PREVIOUS_BUTTON = "";
+						break;
+					case "/":
+						if (y == 0) {
+							result.textContent = "ERROR";
+						}
+						OPERATOR1 = x / y; //Do the operation
+						OPERATOR2 = ""; //Empty out
+						result.textContent = OPERATOR1; //Display
+						currentSign.textContent = ""; //Changes the text so it displays the sign
+						PREVIOUS_BUTTON = "";
+						break;
+				}
+		}
 	}
 }
